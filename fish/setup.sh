413f67f4b571cea73f91fcf503673afdf7258dd7
Martin Kačmar <martin@kacmar.sk>
2017-07-06 17:09:52 +0200

    Authenticate user before updating shells

--- fish/setup.sh
+++ fish/setup.sh
@@ -5,6 +5,8 @@ CHSH_BIN=/usr/bin/chsh
 FISH_BIN=/usr/local/bin/fish
 CURL_BIN=/usr/bin/curl
 
+sudo -v
+
 # Adds fish to supported shells if it's not allowed yet
 $GREP_BIN -q -F $FISH_BIN /etc/shells || echo $FISH_BIN >> /etc/shells
 
