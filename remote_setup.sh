06381086ef9749c39fad048e27c50cb0ace2509f
Martin Kačmar <martin@kacmar.sk>
2017-07-06 16:42:53 +0200

    HTTPS instead of SSH used for cloning when SSH keys are not set-up yet

--- remote_setup.sh
+++ remote_setup.sh
@@ -7,6 +7,6 @@ SH_BIN=/bin/sh
 TARGET_DIR=$HOME/dotfiles
 
 $MKDIR_BIN -p $TARGET_DIR
-$GIT_BIN clone git@github.com:martinkacmar/dotfiles.git $TARGET_DIR
+$GIT_BIN clone https://github.com/martinkacmar/dotfiles.git $TARGET_DIR
 cd $TARGET_DIR
 $SH_BIN ./install.sh
